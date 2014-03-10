035ee2f31380324d096bb60d4cd2ae51c989a393
douggiephresh <dradoye@mail.umw.edu>
2014-03-10 10:05:19 -0400

    Update server.py
    commenting out the extra stuff to make the basic code work

--- server.py
+++ server.py
@@ -9,25 +9,25 @@ app = Flask(__name__)
 def mainIndex():
     return render_template('index.html', selected='Home')
 
-@app.route('/review.html')
-def review():
-    return render_template('review.html')
+#@app.route('/review.html')
+#def review():
+#    return render_template('review.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    r = {'Food': request.form['Food'],
-               'Location': request.form['Location'],
-               'Number': request.form['Number'],
-               'Description': request.form['Description']} 
-    db = utils.db_connect()
-    cur = db.cursor()
-    query = 'INSERT INTO reviews VALUES (\'' + r['Food'] + '\',\'' + r['Location'] + '\',' + r['Number'] + ',\'' + r['Description'] + '\');'
-    cur.execute(query)
-    db.commit()
-    query = 'SELECT * from reviews'
-    cur.execute(query)
-    rows = cur.fetchall()
-    return render_template('reviews.html', reviews=rows, selectedMenu='Reviews')
+#@app.route('/submit', methods=['POST'])
+#def submit():
+#    r = {'Food': request.form['Food'],
+#               'Location': request.form['Location'],
+#               'Number': request.form['Number'],
+#               'Description': request.form['Description']} 
+#    db = utils.db_connect()
+#    cur = db.cursor()
+#    query = 'INSERT INTO reviews VALUES (\'' + r['Food'] + '\',\'' + r['Location'] + '\',' + r['Number'] + ',\'' + r['Description'] + '\');'
+#    cur.execute(query)
+#    db.commit()
+#    query = 'SELECT * from reviews'
+#    cur.execute(query)
+#    rows = cur.fetchall()
+#    return render_template('reviews.html', reviews=rows, selectedMenu='Reviews')
 
 
 @app.route('/reviews.html')
