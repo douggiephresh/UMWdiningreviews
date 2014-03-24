e2fce8c471c65dfb1ae17b2854e4451f116a7818
douggiephresh <dradoye@mail.umw.edu>
2014-03-24 10:43:57 -0400

    Update UMWdiningDB.sql
    Here's the tables in 3NF

--- UMWdiningDB.sql
+++ UMWdiningDB.sql
@@ -8,38 +8,40 @@ USE UMWdining;
 
 CREATE TABLE reviews
 (
-  food VARCHAR(25),
-  locations VARCHAR(25),
-  rating int,
-  summary VARCHAR(144)
+  reviewkey INT() NOT NULL AUTO_INCREMENT,
+  text BLOB(),
+  rating int NOT NULL,
+  PRIMARY KEY (reviewkey)
 );
 
-CREATE TABLE descriptions 
+CREATE TABLE review_food
 (
-	name VARCHAR(35) NOT NULL,
-	picture VARCHAR(255),
-	description VARCHAR(255)
-
+  reviewkey INT(),
+  foodkey INT();
 );
 
-CREATE TABLE menu
+CREATE TABLE foods
 (
-   location VARCHAR(25),
-   items VARCHAR(100)
+   foodkey INT() NOT NULL AUTO_INCREMENT
+   name VARCHAR(25) NOT NULL,
+   description BLOB(),
+   PRIMARY KEY (foodkey)
 );
 
-  
-INSERT INTO reviews VALUES ('Sandwich', 'The Nest', 5, 'The sandwich was a bit dry however I enjoyed the options.');
-INSERT INTO reviews VALUES ('Soup', 'Woodstock', 6, 'Good soup.  Wish they had more varieties.');
-INSERT INTO reviews VALUES ('Salad', 'Seacobeck', 3, 'The variety of toppings is not the problem. There are so many choices!  The food however sits out too long.');
-INSERT INTO reviews VALUES ('Omlette', 'Seacobeck', 9, 'Estelle makes the best omlettes I have ever tasted.');
+CREATE TABLE food_location
+(
+   foodkey INT(),
+   locationkey INT()
+);
+
+CREATE TABLE locations 
+(
+   locationkey INT () NOT NULL AUTO_INCREMENT,
+   name char (24) NOT NULL,
+   description BLOB (),
+   image BLOB (),
+   PRIMARY KEY (locationkey)
+
+);
 
 
-
-INSERT INTO descriptions VALUES ('The Underground', 'A Picture', 'A description');
-INSERT INTO descriptions VALUES ('Seacobeck Dining Hall', 'A PIcture', 'A description');
-INSERT INTO descriptions VALUES ('The Eagles Nest', 'A Pciture', 'A description');
-
-
-INSERT INTO menu VALUES ('SeacoBeck', 'cheeseburger, pasta, chicken');
-INSERT INTO menu VALUES ('The Nest"', 'Buffalo Wrap, Burger, Wings');
